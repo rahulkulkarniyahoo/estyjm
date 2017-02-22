48b294785a835009bf542b4d1a95527139de8529
Rahul <rahulkulkarni@yahoo.com>
2017-02-22 21:24:23 +0000

    added created inventory listing class empty file

---
+++ src/test/java/etsy/CreateInventoryListing.java
@@ -0,0 +1,15 @@
+package etsy;
+
+/**
+ * Created by Rahul on 2017-02-22.
+ */
+public class CreateInventoryListing {
+
+    public class void main(String[] args){
+        //Login to the application
+
+        //Click on Your shop -> Listings -> Add a Listing
+
+    }
+
+}
